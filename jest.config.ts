7b172f5e142d2a60ef5b210f2597720421035fa3
Morg42 <43153739+Morg42@users.noreply.github.com>
2026-05-21 19:14:19 +0200

    update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+  silent: true,
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
